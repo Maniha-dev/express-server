c882e7cd26311660221f31475670cb98322892b9
Maniha-dev <nasirrazaptcl@gmail.com>
2026-07-03 01:25:41 -0700

    Initial commit - basic express server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send(`Heloo, ${req.query.name}`)
+});
+app.get('/about', (req, res) =>{
+    return res.send("/Hello from aboutii!")
+});
+app.get('/services', (req, res) =>{
+    return res.send("/Our service!")
+});
+app.get('/Joinus', (req, res) =>{
+    return res.send("Wanna Join Us!")
+});
+
+app.listen (8000,() => console.log("Sab set chal raa!"));
